d933fd4433c478b6ba2b214df31afcdd886c46d8
jason.maloney@gmail.com <jason.maloney@gmail.com>
2014-01-06 10:27:12 -0600

    Remove if already exists

--- vim/install.sh
+++ vim/install.sh
@@ -5,4 +5,5 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+rm -rf ~/.vim/bundle/git-colors-solarized/
 git clone git@github.com:JasonMaloney/vim-colors-solarized.git ~/.vim/bundle/git-colors-solarized/
