1e0107a1cddaea9371534ba9348ddd0f2385effc
Jason Maloney <jason@localhost.localdomain>
2014-01-06 10:56:24 -0600

    Fix paths

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,4 @@
 
-rm -rf ~/.oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
