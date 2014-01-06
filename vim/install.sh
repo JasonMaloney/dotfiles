853815e0990d9b595a783a87e98da19dee007958
Jason Maloney <jason@localhost.localdomain>
2014-01-06 10:55:36 -0600

    Fix variables

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,9 @@
 
 # Install Pathogen and Solarized theme for vim
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim \
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+curl -Sso $HOME/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-rm -rf ~/.vim/bundle/git-colors-solarized/
-git clone git@github.com:JasonMaloney/vim-colors-solarized.git ~/.vim/bundle/git-colors-solarized/
+rm -rf $HOME/.vim/bundle/git-colors-solarized/
+git clone git@github.com:JasonMaloney/vim-colors-solarized.git $HOME/.vim/bundle/git-colors-solarized/
